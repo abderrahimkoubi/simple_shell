06244165e05bfd5c3825f20bbb987ca186cf6de6
NizarMA <jediyonizar02@gmail.com>
2023-05-24 09:42:20 +0000

    task 1

--- simple_shell_01.c
+++ simple_shell_01.c
@@ -1,82 +1,75 @@
-#include "shell.h"
-/**
- * command - This function return the command
- * @cmd: The command whith \n
- * Return: The command
- */
-char *command(char *cmd)
-{
-	int i, cpt;
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
-	i = 0;
-	cpt = 0;
-	while (cmd[i] != '\0')
-	{
-		cpt++;
-		i++;
-	}
-	cmd[cpt - 1] = '\0';
-	return (cmd);
-}
-/**
- * init_argv - Initialize the argv in NULL
- * @arg: The array of string
- * Return: nothing
- */
-void init_argv(char **arg)
-{
-	int i = 0;
+#define BUFFER_SIZE 1024
 
-	while (i < 4)
-	{
-		arg[i] = NULL;
-		i++;
-	}
-}
+int main(void);
 /**
- * main - The entry point of program
- * @ac: The size of av array
- * @av: The arrat
- * @env: The env the content of the env
- * Return: Always 0.
+ * main - Entry point
+ *
+ * Description: Unique UNIX command line interpreter
+ * Return: Always 0
  */
-int main(int ac, char **av, char **env)
+int main(void)
 {
-	char *argv[] = {NULL, NULL, NULL, NULL};
-	char *cmd = NULL, *str1, *token;
-	size_t len;
-	int i;
-	pid_t myPid;
+	char *buffer = NULL;
+	size_t bufsize = 0;
+	ssize_t characters;
+	char *prompt = "#shell$ ";
 
 	while (1)
 	{
-		printf("#cisfun$ ");
-		if (getline(&cmd, &len, stdin) == -1)
-			break;
-		cmd = command(cmd);
-		if (exit_cmd(cmd) == 0)
-			break;
-		if (env_cmd(cmd) == 0)
-			_env(env);
-		init_argv(argv);
-		for (i = 0, str1 = cmd; ; i++, str1 = NULL)
+		write(STDOUT_FILENO, prompt, strlen(prompt));
+
+		characters = getline(&buffer, &bufsize, stdin);
+		if (characters == -1)
 		{
-			token = strtok(str1, " ");
-			if (token == NULL)
-				break;
-			argv[i] = token;
+			if (isatty(STDIN_FILENO))
+				write(STDOUT_FILENO, "\n", 1);
+			break;
 		}
-		if (argv[2] == NULL)
+
+		if (buffer[characters - 1] == '\n')
+			buffer[characters - 1] = '\0';
+
+		if (strcmp(buffer, "exit") == 0)
 		{
-			execute_process(&myPid, argv, av[ac - 1]);
+			free(buffer);
+			break;
+		}
+
+		if (access(buffer, X_OK) == 0)
+		{
+			pid_t pid = fork();
+
+			if (pid == -1)
+			{
+				perror("fork");
+			}
+			else if (pid == 0)
+			{
+				char *argv[] = {buffer, NULL};
+
+				execve(buffer, argv, NULL);
+				perror("execve");
+				_exit(EXIT_FAILURE);
+			}
+			else
+			{
+				wait(NULL);
+			}
 		}
 		else
 		{
-			argv[3] = NULL;
-			if (access(argv[0], X_OK) == -1)
-				continue;
-			execute_process(&myPid, argv, av[ac - 1]);
+			char *error_message = "error command not found\n";
+
+			write(STDOUT_FILENO, error_message, strlen(error_message));
 		}
 	}
+
 	return (0);
 }
