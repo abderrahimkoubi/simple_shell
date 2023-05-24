7072bc3479c3a897bd1c69780d9b59c04ccccee8
NizarMA <jediyonizar02@gmail.com>
2023-05-24 17:24:15 +0000

    task 1

--- shell.h
+++ shell.h
@@ -1,16 +1,19 @@
 #ifndef SHELL_H
 #define SHELL_H
-
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
 #include <sys/types.h>
-#include <sys/stat.h>
+#include <stdio.h>
+#include <sys/types.h>
 #include <sys/wait.h>
+#include <unistd.h>
 #include <string.h>
 
-int _strlen(const char *s);
-char *str_concat(const char *s1, const char *s2);
-int _strcmp(const char *s1, const char *s2);
-char *_strdup(const char *str);
+void _exec(char **tmp_args, char **env);
+void _find_path(char **env, char **tmp_args, char **buffer);
+void _exec_path(char *tmp_path, char **tmp_args, char **env);
+void _update_PWD(char **env);
+void _handle_cd(char **env, char **tmp_args, char **buffer);
+int _get_user_input(ssize_t n, char inputcmd[100], char *input);
+void _loop(char **buffer, char **env);
+void _handle_args(char **tmp_args, char inputcmd[100]);
+
 #endif
--- simple_shell_01.c
+++ simple_shell_01.c
@@ -1,120 +1,26 @@
 #include "shell.h"
 
 /**
-* _strlen - get length of string
-* @s: A pointer to char
-*
-*  a function that returns the length of a string.
-* Return: length of string
+ * _exec_path - finds the path of a command and executes it.
+ * @tmp_path: path to the command
+ * @tmp_args: array of cmd and args
+ * @env: env variables
+ * Return: Nothing
 */
-int _strlen(const char *s)
+
+void _exec_path(char *tmp_path, char **tmp_args, char **env)
 {
-	int len = 0;
-	const char *c = s;
+	int status;
+	pid_t childPid;
 
-	while (*c != '\0')
+	childPid = fork();
+	if (childPid == 0)
 	{
-		len++;
-		c++;
+		tmp_args[0] = strdup(tmp_path);
+		execve(tmp_args[0], tmp_args, env);
 	}
-
-	return (len);
-}
-
-/**
- * str_concat - concatenates two strings.
- * @s1: first string
- * @s2: second string
- *
- * if NULL is passed, treat it as an empty string
- * Return: a pointer should point to a newly allocated space
- * in memory which contains the ontents of s1,
- * followed by the contents of s2, and null terminated
- * or return NULL on failure
- */
-char *str_concat(const char *s1, const char *s2)
-{
-	char *concat;
-	int size1 = 0;
-	int size2 = 0;
-	int i = 0;
-	int j;
-
-	if (s1 != NULL)
+	else
 	{
-		while (s1[size1] != '\0')
-			size1++;
+	wait(&status);
 	}
-	if (s2 != NULL)
-	{
-		while (s2[size2] != '\0')
-			size2++;
-	}
-	concat = malloc((size1 + size2 + 1) * sizeof(char));
-	if (concat == NULL)
-		return (NULL);
-
-	if (s1 != NULL)
-	{
-		for (i = 0; i < size1; i++)
-			concat[i] = s1[i];
-	}
-	if (s2 != NULL)
-	{
-		for (j = 0; i < size1 + size2; i++, j++)
-			concat[i] = s2[j];
-	}
-	concat[i] = '\0';
-	return (concat);
-}
-
-/**
- * _strcmp - compares two strings.
- * @s1: first string
- * @s2: second string
- *
- *  a function that compares two strings similar to strcmp funcution
- *  Return: 0: s1 = s2, +ve value: s1 > s2, -ve value: s1 < s2
- */
-int _strcmp(const char *s1, const char *s2)
-{
-	int i;
-
-	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
-	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
-	}
-
-	return (s1[i] - s2[i]);
-}
-
-/**
- * _strdup - copy string
- * @str: a string to be copied
- * a function that returns a pointer to a newly allocated space in memory
- * which contains a copy of the string given as a parameter.
- *
- * Return: a pointer to a new string which is a duplicate of the string str
- * or returns NULL if insufficient memory was available
- */
-char *_strdup(const char *str)
-{
-	char *copy;
-	int size = 0;
-	int i;
-
-	if (str == NULL)
-		return (NULL);
-
-	while (str[size] != '\0')
-		size++;
-	size++;
-	copy = malloc(size * sizeof(char));
-	if (copy == NULL)
-		return (NULL);
-
-	for (i = 0; i < size; i++)
-		copy[i] = str[i];
-	return (copy);
 }
