4c28fbcedf6f8357ed3605ea461ce168b6a527e8
NizarMA <jediyonizar02@gmail.com>
2023-05-24 18:06:19 +0000

    task 1

--- shell.h
+++ shell.h
@@ -1,6 +1,16 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define TOKEN_BUF_SIZE 64
+#define TOKEN_DELIM " \t\r\n\a"
+
 void start_prompt(void);
 char *read_command(void);
 char **get_array_from_str(char *str, char **env);
