fda85005463d828867eda9359757cd37a3e9318f
NizarMA <jediyonizar02@gmail.com>
2023-05-24 17:04:50 +0000

    task 1

--- simple_shell_01.c
+++ simple_shell_01.c
@@ -1,79 +1,120 @@
 #include "shell.h"
 
 /**
- * _strlen - Returns the length of a string.
- * @s: The string to calculate the length of.
- *
- * Return: The length of the string.
- */
-int _strlen(char *s)
+* _strlen - get length of string
+* @s: A pointer to char
+*
+*  a function that returns the length of a string.
+* Return: length of string
+*/
+int _strlen(const char *s)
 {
 	int len = 0;
+	const char *c = s;
 
-	if (!s)
-		return (0);
-
-	while (s[len])
+	while (*c != '\0')
+	{
 		len++;
+		c++;
+	}
 
 	return (len);
 }
 
 /**
- * _strcmp - Performs lexicographic comparison of two strings.
- * @s1: The first string to compare.
- * @s2: The second string to compare.
+ * str_concat - concatenates two strings.
+ * @s1: first string
+ * @s2: second string
  *
- * Return: Negative if s1 < s2, positive if s1 > s2, 0 if s1 == s2.
+ * if NULL is passed, treat it as an empty string
+ * Return: a pointer should point to a newly allocated space
+ * in memory which contains the ontents of s1,
+ * followed by the contents of s2, and null terminated
+ * or return NULL on failure
  */
-int _strcmp(char *s1, char *s2)
+char *str_concat(const char *s1, const char *s2)
 {
-	while (*s1 && *s1 == *s2)
-	{
-		s1++;
-		s2++;
-	}
+	char *concat;
+	int size1 = 0;
+	int size2 = 0;
+	int i = 0;
+	int j;
 
-	return (*(unsigned char *)s1 - *(unsigned char *)s2);
+	if (s1 != NULL)
+	{
+		while (s1[size1] != '\0')
+			size1++;
+	}
+	if (s2 != NULL)
+	{
+		while (s2[size2] != '\0')
+			size2++;
+	}
+	concat = malloc((size1 + size2 + 1) * sizeof(char));
+	if (concat == NULL)
+		return (NULL);
+
+	if (s1 != NULL)
+	{
+		for (i = 0; i < size1; i++)
+			concat[i] = s1[i];
+	}
+	if (s2 != NULL)
+	{
+		for (j = 0; i < size1 + size2; i++, j++)
+			concat[i] = s2[j];
+	}
+	concat[i] = '\0';
+	return (concat);
 }
 
 /**
- * starts_with - Checks if a string starts with a given substring.
- * @haystack: The string to search in.
- * @needle: The substring to find.
+ * _strcmp - compares two strings.
+ * @s1: first string
+ * @s2: second string
  *
- * Return: Address of the next character after the substring if found,
- *         NULL otherwise.
+ *  a function that compares two strings similar to strcmp funcution
+ *  Return: 0: s1 = s2, +ve value: s1 > s2, -ve value: s1 < s2
  */
-char *starts_with(const char *haystack, const char *needle)
+int _strcmp(const char *s1, const char *s2)
 {
-	while (*needle)
+	int i;
+
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
-		if (*haystack != *needle)
-			return (NULL);
-		haystack++;
-		needle++;
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
 	}
 
-	return ((char *)haystack);
+	return (s1[i] - s2[i]);
 }
 
 /**
- * _strcat - Concatenates two strings.
- * @dest: The destination string.
- * @src: The source string.
+ * _strdup - copy string
+ * @str: a string to be copied
+ * a function that returns a pointer to a newly allocated space in memory
+ * which contains a copy of the string given as a parameter.
  *
- * Return: Pointer to the destination string.
+ * Return: a pointer to a new string which is a duplicate of the string str
+ * or returns NULL if insufficient memory was available
  */
-char *_strcat(char *dest, char *src)
+char *_strdup(const char *str)
 {
-	char *ret = dest;
+	char *copy;
+	int size = 0;
+	int i;
 
-	while (*dest)
-		dest++;
+	if (str == NULL)
+		return (NULL);
 
-	while ((*dest++ = *src++))
-		;
+	while (str[size] != '\0')
+		size++;
+	size++;
+	copy = malloc(size * sizeof(char));
+	if (copy == NULL)
+		return (NULL);
 
-	return (ret);
+	for (i = 0; i < size; i++)
+		copy[i] = str[i];
+	return (copy);
 }
