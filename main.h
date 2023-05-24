350dbbf5c3829eacabf83aec47a370b0bcea37c6
NizarMA <jediyonizar02@gmail.com>
2023-05-24 16:03:13 +0000

    task 1

--- main.h
+++ main.h
@@ -11,26 +11,10 @@
 #include <dirent.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
-typedef struct builtin
-
-extern char **environ;
-
-int _strlen(char *);
-int _putchar(char);
-int _strcmp(char *, char *);
-
-char *_strdup(char *);
-void _printstr(char *, int);
-int print_num(int nin);
-void free_db(char **);
-void free_sng(int, ...);
-void print_err(char *, int, char *);
-void handle_exec_error(char *, int, char *);
-
-void (*builtin_handler(char *))(char *);
-void cmd_env(char *);
-void cmd_exit(char *);
-int execute_builtin(char **, char *);
-
+int execute_builtin(data *data)
+void exit_builtin(data *data)
+void env_builtin(data *data)
+void setenv_builtin(data *data)
+void unsetenv_builtin(data *data)
 
 #endif
--- simple_shell_01.c
+++ simple_shell_01.c
@@ -1,101 +1,106 @@
 #include "main.h"
 
 /**
- * _strlen - returns length of a string
- * @s: takes input
- * Return: Always 0
-*/
-int _strlen(char *s)
-{
-	int len;
-
-	if (s == NULL)
-	{
-		return (0);
-	}
-	for
-		(len = 0;
-		 s[len] != '\0';
-		 len++);
-	return (len);
-}
-
-/**
- * _putchar - Writes a character to stdout
- * @c: Character that will be written to stdout
- * Return: Upon success how many characters were written.
+ * execute_builtin - Check if it is a built-in command and then execute it.
+ * @data: Data structure input.
+ * Return: 1 if it is a built-in command, 0 if not.
  */
-
-int _putchar(char c)
+int execute_builtin(data *data)
 {
-	return (write(1, &c, 1));
-}
+	builtin builtins[] = {
+		{"exit_shell", exit_builtin},
+		{"show_env", env_builtin},
+		{"set_env", setenv_builtin},
+		{"unset_env", unsetenv_builtin},
+		{"change_dir", cd_builtin},
+		{NULL, NULL},
+	};
+	int index = 0;
 
-/**
- * _strcmp - compares two strings
- * @a: First string
- * @b: Second string
- * Return: 0 if strings match. -1 Otherwise.
- */
-
-int _strcmp(char *a, char *b)
-{
-	int _i;
-
-	if (_strlen(a) != _strlen(a))
+	for (index = 0; builtins[index].cmd; index++)
 	{
-		return (-1);
-	}
-	for (_i = 0; a[_i] != '\0'; _i++)
-	{
-		if (a[_i] != b[_i])
+		if (_strcmp(data->arguments[0], builtins[index].cmd) == 0)
 		{
-			return (-1);
+			builtins[index].func(data);
+			return (1);
 		}
 	}
 	return (0);
 }
+
 /**
-* _strdup - duplicates a string
-* @start: original string
-* Return: copy of string
-*/
-
-char *_strdup(char *start)
+ * exit_builtin - Exit the shell.
+ * @data: Data structure input.
+ * Return: void
+ */
+void exit_builtin(data *data)
 {
-	int index, length;
-	char *end;
-
-	length = _strlen(start);
-	end = malloc(sizeof(char) * (length + 1));
-	for (index = 0; start[index] != '\0'; index++)
-	{
-		end[index] = start[index];
-	}
-	end[index] = '\0';
-	return (end);
+	if (data->arguments[1] && _is_number(data->arguments[1]))
+		data->exit_status = atoi(data->arguments[1]);
+	free_array(data->arguments);
+	free(data->command);
+	if (data->flag_setenv)
+		free_array(environ);
+	exit(data->exit_status);
 }
 
 /**
- * _printstr - Prints a string character by character
- * @s: String to be printed
- * @n: if integer is 0 a new line will be printed
+ * env_builtin - Print the current environment.
+ * @data: Data structure input.
+ * Return: void
  */
-
-void _printstr(char *s, int n)
+void env_builtin(data *data)
 {
-	int ret;
+	int i = 0;
 
-	if (s == NULL)
+	(void)data;
+	while (environ[i])
 	{
-		s = "(null)";
+		_printf(environ[i]);
+		_printf("\n");
+		i++;
 	}
-	for (ret = 0; s[ret] != '\0'; ret++)
+}
+
+/**
+ * setenv_builtin - Initialize a new environment var
+ * or modify the existing one.
+ * @data: Data structure input.
+ * Return: void
+ */
+void setenv_builtin(data *data)
+{
+	(void)data;
+	if (data->arguments[1] && data->arguments[2])
 	{
-		write(STDOUT_FILE NO, &s[ret], 1);
+		if (_setenv(data, data->arguments[1], data->arguments[2]) == -1)
+		{
+			perror("setenv");
+		}
 	}
-	if (n == 0)
+}
+
+/**
+ * unsetenv_builtin - Remove an environment variable.
+ * @data: Data structure input.
+ * Return: void
+ */
+void unsetenv_builtin(data *data)
+{
+	int i, j;
+	int len;
+
+	(void)data;
+	if (!data->arguments[1] || !getenv(data->arguments[1]))
 	{
-		write(STDOUT_FILE NO, "\n", 1);
+		_perror(data->shell_name, "variable not found.");
+		return;
 	}
+	len = strlen(data->arguments[1]);
+	for (i = 0; environ[i]; i++)
+		if
+			(strncmp(environ[i], data->arguments[1], len) == 0
+			 && environ[i][len] == '=')
+			for (j = i; environ[j]; j++)
+				environ[j] = environ[j + 1];
 }
