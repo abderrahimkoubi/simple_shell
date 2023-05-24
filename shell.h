586c811f7be3137e4a85195e9ce5f5805a421cad
NizarMA <jediyonizar02@gmail.com>
2023-05-24 17:13:39 +0000

    task 1

--- shell.h
+++ shell.h
@@ -1,20 +1,16 @@
-#ifndef _SHELL_H_
-#define _SHELL_H_
+#ifndef SHELL_H
+#define SHELL_H
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <unistd.h>
-#include <string.h>
 #include <sys/types.h>
-#include <sys/wait.h>
 #include <sys/stat.h>
-#include <limits.h>
-#include <fcntl.h>
-#include <errno.h>
+#include <sys/wait.h>
+#include <string.h>
 
-int _strlen(char *);
-int _strcmp(char *, char *);
-char *starts_with(const char *, const char *);
-char *_strcat(char *, char *);
-
+int _strlen(const char *s);
+char *str_concat(const char *s1, const char *s2);
+int _strcmp(const char *s1, const char *s2);
+char *_strdup(const char *str);
 #endif
