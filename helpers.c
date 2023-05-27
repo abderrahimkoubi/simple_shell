6a32f04fa04903be6a87db4e458f85a8eb53eb34
NizarMA <jediyonizar02@gmail.com>
2023-05-27 02:08:17 +0000

    tasks

--- helpers.c
+++ helpers.c
@@ -1,57 +1,159 @@
 #include "shell.h"
 
-/**
- * read_input - Reads a line of input from the user.
- *
- * Return: The input string read from the user.
- */
-char *read_input()
-{
-char *user_input = NULL;
-size_t bufsize = 0;
+void free_args(char **args, char **front);
+char *get_pid(void);
+char *get_env_value(char *beginning, int len);
+void variable_replacement(char **args, int *exe_ret);
 
-getline(&user_input, &bufsize, stdin);
-	return (user_input);
+/**
+ * free_args - Frees up memory taken by args.
+ * @args: A null-terminated double pointer containing commands/arguments.
+ * @front: A double pointer to the beginning of args.
+ */
+
+void free_args(char **args, char **front)
+{
+	size_t i;
+
+	for (i = 0; args[i] || args[i + 1]; i++)
+		free(args[i]);
+
+	free(front);
 }
 
 /**
- * split_input - Splits the input string into an array of tokens.
- * @user_input: The input string to be split.
+ * get_pid - Gets the current process ID.
+ * Description: Opens the stat file, a space-delimited file containing
+ *              information about the current process. The PID is the
+ *              first word in the file. The function reads the PID into
+ *              a buffer and replace the space at the end with a \0 byte.
  *
- * Return: An array of tokens.
+ * Return: The current process ID or NULL on failure.
  */
-char **split_input(char *user_input)
+
+char *get_pid(void)
 {
-int token_bufsize = TOKEN_BUFFER_SIZE;
-int position = 0;
-char **tokens = malloc(token_bufsize * sizeof(char *));
-char *token;
+	size_t i = 0;
+	char *buffer;
+	ssize_t file;
 
-	if (!tokens)
+	file = open("/proc/self/stat", O_RDONLY);
+	if (file == -1)
 	{
-	fprintf(stderr, "Allocation error\n");
-	exit(EXIT_FAILURE);
+		perror("Cant read file");
+		return (NULL);
+	}
+	buffer = malloc(120);
+	if (!buffer)
+	{
+		close(file);
+		return (NULL);
+	}
+	read(file, buffer, 120);
+	while (buffer[i] != ' ')
+		i++;
+	buffer[i] = '\0';
+
+	close(file);
+	return (buffer);
+}
+
+/**
+ * get_env_value - Gets the value corresponding to an environmental variable.
+ * @beginning: The environmental variable to search for.
+ * @len: The length of the environmental variable to search for.
+ *
+ * Return: If the variable is not found - an empty string.
+ *         Otherwise - the value of the environmental variable.
+ *
+ * Description: Variables are stored in the format VARIABLE=VALUE.
+ */
+
+char *get_env_value(char *beginning, int len)
+{
+	char **var_addr;
+	char *replacement = NULL, *temp, *var;
+
+	var = malloc(len + 1);
+	if (!var)
+		return (NULL);
+	var[0] = '\0';
+	_strncat(var, beginning, len);
+
+	var_addr = _get_env(var);
+	free(var);
+	if (var_addr)
+	{
+		temp = *var_addr;
+		while (*temp != '=')
+			temp++;
+		temp++;
+		replacement = malloc(_strlen(temp) + 1);
+		if (replacement)
+			_strcpy(replacement, temp);
 	}
 
-	token = strtok(user_input, TOKEN_DELIMITERS);
-	while (token != NULL)
-	{
-	tokens[position] = token;
-	position++;
+	return (replacement);
+}
 
-	if (position >= token_bufsize)
-	{
-	token_bufsize += TOKEN_BUFFER_SIZE;
-	tokens = realloc(tokens, token_bufsize * sizeof(char *));
-	if (!tokens)
-	{
-		fprintf(stderr, "Allocation error\n");
-		exit(EXIT_FAILURE);
-	}
-	}
+/**
+ * variable_replacer - Handles variable replacement.
+ * @line: A double pointer containing the command and arguments.
+ * @exe_ret: A pointer to the return value of the last executed command.
+ *
+ * Description: Replaces $$ with the current PID, $? with the return value
+ *              of the last executed program, and envrionmental variables
+ *              preceded by $ with their corresponding value.
+ */
 
-	token = strtok(NULL, TOKEN_DELIMITERS);
+void variable_replacer(char **line, int *exe_ret)
+{
+	int j, k = 0, len;
+	char *replacement = NULL, *old_line = NULL, *new_line;
+
+	old_line = *line;
+	for (j = 0; old_line[j]; j++)
+	{
+		if (old_line[j] == '$' && old_line[j + 1] &&
+				old_line[j + 1] != ' ')
+		{
+			if (old_line[j + 1] == '$')
+			{
+				replacement = get_pid();
+				k = j + 2;
+			}
+			else if (old_line[j + 1] == '?')
+			{
+				replacement = _itoa(*exe_ret);
+				k = j + 2;
+			}
+			else if (old_line[j + 1])
+			{
+				/* extract the variable name to search for */
+				for (k = j + 1; old_line[k] &&
+						old_line[k] != '$' &&
+						old_line[k] != ' '; k++)
+					;
+				len = k - (j + 1);
+				replacement = get_env_value(&old_line[j + 1], len);
+			}
+			new_line = malloc(j + _strlen(replacement)
+					  + _strlen(&old_line[k]) + 1);
+			if (!line)
+				return;
+			new_line[0] = '\0';
+			_strncat(new_line, old_line, j);
+			if (replacement)
+			{
+				_strcat(new_line, replacement);
+				free(replacement);
+				replacement = NULL;
+			}
+			_strcat(new_line, &old_line[k]);
+			free(old_line);
+			*line = new_line;
+			old_line = new_line;
+			j = -1;
+		}
 	}
-	tokens[position] = NULL;
-	return (tokens);
 }
